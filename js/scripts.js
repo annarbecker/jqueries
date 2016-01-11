6f664239eb16001085e49bf95cef077f8929824d
Anna Becker and Sara Wagner <student@epicodus.com>
2016-01-11 14:28:29 -0800

    Add more jQuery elements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ jQuery(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
   });
-  
-  jQuery("h2").dblclick(function() {
-    alert("This is a second header.");
-  });
 
   jQuery(".clickable").click(function() {
     $("#initially-showing").toggle();
     $("#initially-hidden").toggle();
+  });
+
+  jQuery(".clickable2").click(function() {
+    $("#hide-text").slideUp();
+    $("#show-text").slideToggle();
   });
 });
