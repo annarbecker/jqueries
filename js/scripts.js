ff3a84888879fc62bb28ee98ef9429557837c6c7
Anna Becker and Sara Wagner <sara.wagner23q@gmail.com>
2016-01-11 13:49:50 -0800

    Add jQuery elements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,13 @@ jQuery(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
   });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
+  
   jQuery("h2").dblclick(function() {
     alert("This is a second header.");
   });
 
-  jQuery("img").hover(function() {
-    alert("LOOK AT THESE PIGS!");
+  jQuery(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
